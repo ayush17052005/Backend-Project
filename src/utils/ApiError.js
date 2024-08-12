b5da64ceb5e9132a091fc8f3f78a4fe32dbfd9e2
ayush17052005 <ayushsaha1705@gmail.com>
2024-08-12 18:07:12 +0530

    fixed some errors

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -8,12 +8,13 @@ class ApiError extends Error {
     super(message)
     this.statusCode = statusCode
     this.data = null
-    this.message = false
+    this.message=message
+    this.success = false
     this.errors = errors
     if (stack) {
       this.stack=stack
     } else {
-      Error.captureStackTree(this,this.constructor)
+      Error.captureStackTrace(this,this.constructor)
     }
   }
 }
